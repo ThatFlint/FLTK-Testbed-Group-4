a6bc0b29101c9bf01947f51a5c17be33ca6ec585
Yujin-Zhu <yujin.zhu3@gmail.com>
2021-06-07 22:34:31 +0200

    Calculate lambda_t

--- fltk/util/update_dist.py
+++ fltk/util/update_dist.py
@@ -1,4 +1,4 @@
-'''Update the distribution of configurations and calculate the cross entropy'''
+'''Update the distribution of configurations and calculate the entropy'''
 
 import math
 
@@ -7,7 +7,7 @@ import math
 # 2. configs, the possible configurations
 # 3. chosen_configs, the configurations chosen by clients in one epoch
 # 4. losses: loss sent by each client
-# 5. V: size of the test data size of each client
+# 5. V: size of the validation data size of each client
 def update_dist(dist, configs, chosen_configs, losses, V):
     new_dist = []
     for p in dist:
@@ -21,11 +21,20 @@ def update_dist(dist, configs, chosen_configs, losses, V):
     for v in V:
         sum_v += v
 
+    # Calculate the sum of the losses
+    sum_loss = 0
+    for loss in losses:
+        sum_loss += loss
+
+    # Calculate the baseline to reduce the variance of grads
+    lambda_t = sum_loss/sum_v
+
     # Calculate gradient
     for j in range(len(configs)):
         for i in range(len(chosen_configs)):
             if chosen_configs[i] == configs[j]:
                 grads[j] += factor*losses[i]*V[i]/(dist[j]*sum_v)
+                # grads[j] += (losses[i]-lambda_t)*V[i] / (dist[j]*sum_v)
 
     # Update distribution
     sum_p = 0
@@ -49,7 +58,7 @@ if __name__ == "__main__":
     dist = [0.2, 0.2, 0.2, 0.2, 0.2]
     configs = [10, 16, 32, 64, 128]
     chosen_configs = [10, 128, 64, 64, 16]
-    losses = [600, 200, 350, 400, 500]
+    losses = [90, 100, 85, 79, 60]
     V = [10, 10, 10, 10, 10]
     new_dist = update_dist(dist, configs, chosen_configs, losses, V)
     old_entropy = cal_dist_entropy(dist)
