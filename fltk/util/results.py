3220b115193ebbdcf9e24aa34d8729f9b726bfa6
Yujin-Zhu <yujin.zhu3@gmail.com>
2021-06-13 09:09:06 +0200

    Remove dist from epochData

--- fltk/util/results.py
+++ fltk/util/results.py
@@ -13,8 +13,8 @@ class EpochData:
     class_recall: Any
     batch_size: int
     test_datasize: int
-    dist: Any
-    chosen: int # index of chosen configuration tuple in configs/dist
+    # dist: Any
+    chosen_config_index: int # index of chosen configuration tuple in configs/dist
     client_id: str = None
 
     def to_csv_line(self):
