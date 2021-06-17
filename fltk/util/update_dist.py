4ff0c92c44c148987af7d0c106fd296130bec745
Yujin-Zhu <yujin.zhu3@gmail.com>
2021-06-17 12:01:49 +0200

    Adjust parameter

--- fltk/util/update_dist.py
+++ fltk/util/update_dist.py
@@ -13,7 +13,6 @@ def update_dist(dist, configs, chosen_configs, losses, V, max_grads, dist_lr_typ
     # 7. dist_lr_type: type of learning rate for the distribution of configurations
     
     new_dist = [p for p in dist]
-    factor = 0.005    # A multiplying factor to reduce the variance of grads
     sum_v = sum(V)
     sum_loss = sum(losses) 
     lambda_t = sum_loss/sum_v # A baseline to reduce the variance of the gradients 
@@ -23,14 +22,13 @@ def update_dist(dist, configs, chosen_configs, losses, V, max_grads, dist_lr_typ
     for j in range(len(configs)):
         for i in range(len(chosen_configs)):
             if chosen_configs[i] == configs[j]:
-                # grads[j] += factor*losses[i]*V[i]/(dist[j]*sum_v)
                 grads[j] += (losses[i]-lambda_t)*V[i] / (dist[j]*sum_v)
     max_grad = max(grads)
     max_grads.append(max_grad)
 
     # Calculate the learning rate of the probabilities
     if (dist_lr_type == "constant"):
-        dist_lr = math.sqrt(2*math.log10(len(configs)))/200
+        dist_lr = math.sqrt(2*math.log10(len(configs)))/400
     elif (dist_lr_type == "adaptive"):
         dist_lr = math.sqrt(2*math.log10(len(configs)))/math.sqrt(sum([grad**2 for grad in max_grads]))
     elif (dist_lr_type == "aggressive"):
@@ -59,7 +57,8 @@ if __name__ == "__main__":
     losses = [90, 100, 85, 79, 60]
     V = [10, 10, 10, 10, 10]
     max_grads = [100]
-    new_dist, max_grads = update_dist(dist, configs, chosen_configs, losses, V, max_grads)
+    dist_lr_type = 'constant'
+    new_dist, max_grads = update_dist(dist, configs, chosen_configs, losses, V, max_grads, dist_lr_type)
     print(new_dist)
     entropy = cal_dist_entropy(new_dist)
 
